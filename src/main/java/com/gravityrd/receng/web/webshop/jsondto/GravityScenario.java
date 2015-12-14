b18bb5f7dead52a02810eeed09e2d582bc01807e
Gabor Bernat <gabor.bernat@gravityrd.com>
2015-12-14 17:49:02 +0100

    allow overriding scenario fields

--- src/main/java/com/gravityrd/receng/web/webshop/jsondto/GravityScenario.java
+++ src/main/java/com/gravityrd/receng/web/webshop/jsondto/GravityScenario.java
@@ -6,46 +6,47 @@ package com.gravityrd.receng.web.webshop.jsondto;
 public class GravityScenario {
 
 	/**
+	 * Holds an internal number identification.
+	 */
+	public int id;
+
+
+	/**
 	 * An unique id usable when interacting with the API.
 	 */
-	public final String apiName;
+	public String apiName;
 
 	/**
 	 * A human readable name for the scenario.
 	 */
-	public final String humanReadableName;
+	public String humanReadableName;
 
 	/**
 	 * A more verbose description about the API.
 	 */
-	public final String description;
+	public String description;
 
 	/**
 	 * On the DASH the scenarios may have set a custom order, this holds their current position.
 	 */
-	public final int order;
+	public int order;
 
 	/**
 	 * This number is the upper limit of items the scenario may return upon recommendation request.
 	 */
-	public final int numberLimit;
+	public int numberLimit;
 
 	/**
 	 * A string token describing the type of the scenario, such as web, ad, mail and so on.
 	 */
-	public final String type;
+	public String type;
 
 	/**
-	 * Allows creating a new scenario object.
+	 * Holds the name of the last modifier.
 	 */
-	GravityScenario(final String apiName, final String humanReadableName, final String description, final String type, final int order, final int numberLimit) {
-		this.apiName = apiName;
-		this.humanReadableName = humanReadableName;
-		this.description = description;
-		this.type = type;
-		this.order = order;
-		this.numberLimit = numberLimit;
-	}
+	public String modifierName;
+
+	public GravityScenario(){}
 
 	/**
 	 * Returns a string representation of the object. In general, the toString method returns a string that "textually represents" this object.
@@ -54,12 +55,14 @@ public class GravityScenario {
 	@Override
 	public String toString() {
 		final StringBuilder sb = new StringBuilder("GravityScenario{");
-		sb.append("apiName='").append(apiName).append('\'');
+		sb.append("id=").append(id);
+		sb.append(", apiName='").append(apiName).append('\'');
 		sb.append(", humanReadableName='").append(humanReadableName).append('\'');
 		sb.append(", description='").append(description).append('\'');
 		sb.append(", order=").append(order);
 		sb.append(", numberLimit=").append(numberLimit);
 		sb.append(", type='").append(type).append('\'');
+		sb.append(", modifierName='").append(modifierName).append('\'');
 		sb.append('}');
 		return sb.toString();
 	}
